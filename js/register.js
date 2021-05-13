e4d7df7a6ec2f54c6255ed89fb825223c90b208b
nando <fmfernando61@gmail.com>
2021-05-13 19:00:49 -0300

    upload

---
+++ js/register.js
@@ -0,0 +1,39 @@
+import validation from './validation.js';
+
+const form = document.querySelector('form');
+const fields = form.querySelectorAll("input");
+
+for ( let field of fields ) {  
+  if (!validation[field.id]) {
+    field.addEventListener("blur", ({ target }) => {
+      validation["default"](field.value, field.id);
+    });
+    continue;
+  }
+  
+  field.addEventListener("blur", ({ target }) => {
+    validation[field.id](field.value, field.id);
+  });
+}
+
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  let error = false;
+
+  for (let field of fields) {
+    if (!validation[field.id]) {
+      if (!validation["default"](field.value, field.id)) error = true;
+      continue;
+    }
+    
+    
+    if (!validation[field.id](field.value, field.id)) error = true;
+  }
+
+  return alert(
+    error
+      ? "Preencha todos os campos corretamente!"
+      : "Conta criada com sucesso!"
+  )
+};
